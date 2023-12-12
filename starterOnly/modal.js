36c8faeb00e8617dad078fe6dbd9ff326adc9e50
Yunita Dwi Liandarwati <ydliandarwati@gmail.com>
2023-12-13 00:41:38 +0100

    validation birthdate and emaile

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -26,26 +26,63 @@ closeBtn.addEventListener("click", () => modalbg.style.display = "none");
 // form elements
 let firstNameField = document.getElementById("first");
 let lastNameField  = document.getElementById("last");
-let email = document.getElementById("email");
+let emailField = document.getElementById("email");
+let birthdateField = document.getElementById('birthdate');
+
+// validate form elements
+firstNameField.addEventListener('change', () => checkName(firstNameField, message.name));
+lastNameField.addEventListener('change', () => checkName(lastNameField, message.name));
+emailField.addEventListener("change", () => checkEmail(emailField, message.email));
+birthdateField.addEventListener("change", () => checkBirthdate(birthdateField, message.birthdate));
+
 
 
 const message = {
   name: "Veuillez entrer 2 caractères ou plus pour le champ du nom.",
   email: "Veuillez renseigner une adresse mail valide.",
+  birthdate : "Vous devez ...",
   city: "Vous devez choisir une option.",
   conditions: "Vous devez vérifier que vous acceptez les termes et conditions."
 };
 
-const checkName = (nameField, msg) => {
-  const name = nameField.value.trim();
+const checkName = (input, msg) => {
+  const name = input.value.trim();
   if (name.length < 2) {
-    setErrorMessage(nameField, msg);
-    return false;
+    setErrorMessage(input, msg);
+    return;
   }
-  hideErrorMessage(nameField);
-  return true; 
+  hideErrorMessage(input);
+  return;
 }
 
+// check email simply using Constraint Validation API
+const checkEmail = (input, msg) => {
+  if (input.validity.typeMismatch) {
+    setErrorMessage(input, msg);
+    return;
+  }
+  hideErrorMessage(input);
+  return;
+}
+
+
+const checkBirthdate = (input, msg) => {
+  const birthdate = new Date(input.value);
+  const currDate =  Date.now();
+  let difference = currDate - birthdate.getTime();
+  diff = new Date(difference);
+  const userAge = diff.getFullYear() - 1970;
+  alert(userAge)
+  const currentYear = new Date().getFullYear();
+  const birthYear = birthdate.getFullYear();
+  
+  if (birthYear < currentYear - 100 || birthYear.toString().length !== 4 || userAge < 18) {
+      setErrorMessage(input, msg);
+      return;
+  } 
+  hideErrorMessage(input);
+  return;
+};
 
 const setErrorMessage = (element, msg) => {
   element.parentElement.setAttribute('data-error-visible', 'true');
@@ -59,19 +96,3 @@ const hideErrorMessage = element => {
 
 
 
-// validate form elements
-firstNameField.addEventListener('change', () => checkName(firstNameField, message.name));
-lastNameField.addEventListener('change', () => checkName(lastNameField, message.name));
-
-
-
-
-
-// check email simply using Constraint Validation API
-// email.addEventListener("change", () => {
-//   if (email.validity.typeMismatch) {
-//     email.setCustomValidity("I am expecting an email address!");
-//   } else {
-//     email.setCustomValidity("");
-//   }
-// });
