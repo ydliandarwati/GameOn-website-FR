34f558c930721db5b8346f11486f4aeb2fc96f83
Yunita Dwi Liandarwati <ydliandarwati@gmail.com>
2023-12-18 00:34:56 +0100

    fixed email and number/quantity using regex

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -37,8 +37,8 @@ const conditionsBtn = document.getElementById('checkbox1');
 firstNameField.addEventListener('change', () => checkName(firstNameField, message.name));
 lastNameField.addEventListener('change', () => checkName(lastNameField, message.name));
 emailField.addEventListener('change', () => checkEmail(emailField, message.email));
-birthdateField.addEventListener('change', () => checkBirthdate(birthdateField, message.birthdate));
-quantityField.addEventListener('change', () => checkQuantity(quantityField, message.quantity));
+birthdateField.addEventListener('input', () => checkBirthdate(birthdateField, message.birthdate));
+quantityField.addEventListener('input', () => checkQuantity(quantityField, message.quantity));
 cityBtn.forEach(btn => btn.addEventListener('change', () => checkCity(cityBtn, message.city)));
 conditionsBtn.addEventListener('change', () => checkConditions(conditionsBtn, message.conditions));
 
@@ -57,22 +57,24 @@ const checkName = (element, msg) => {
   const name = element.value.trim();
   if (name.length < 2) {
     setErrorMessage(element, msg);
-    return;
+    return false;
   }
   hideErrorMessage(element);
-  return;
+  return true;
 }
 
-// check email simply using Constraint Validation API
-// TODO to complete the check
+// check email simply using regular expression RegExp\
+// old version: using Constraint Validation API: element.validity.typeMismatch
 const checkEmail = (element, msg) => {
-  if (element.validity.typeMismatch) {
+  const emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+");
+  if (!emailRegExp.test(element.value)) {
     setErrorMessage(element, msg);
-    return;
+    return false;
   }
   hideErrorMessage(element);
-  return;
+  return true;
 }
+
 
 // check if date is valid or no
 // empty is also invalid (because of required?)
@@ -80,22 +82,26 @@ const checkBirthdate = (element, msg) => {
   const birthdate = new Date(element.value);
   if (isNaN(birthdate) || (element.value.toString().length !== 10)) {
       setErrorMessage(element, msg);
-      return;
+      return false;
   } 
   hideErrorMessage(element);
-  return;
+  return true;
 };
 
 // check quantity value (between 0 and 99)
 // TODO need to add check for invalid inputs (string, empty)
 const checkQuantity = (element, msg) => {
-  const qty = element.value;
-  if (qty>99 || qty<0) {
+  // const qty = element.value;
+  const qtyRegExp = new RegExp("[0-9]{1,2}");
+
+  // const regexQuantity = /^([0-9]{1,2})$/;
+  if(!qtyRegExp.test(element.value)){
+  // if (qty>99 || qty<0 || isNaN(parseInt(qty))) {
     setErrorMessage(element, msg);
-    return;
+    return false;
   }
   hideErrorMessage(element);
-  return;
+  return true;
 }
 
 // check if a city is chosen
@@ -104,20 +110,20 @@ const checkCity = (element, msg) => {
   const isChecked = Array.from(element).some(btn => btn.checked);
   if (!isChecked) {
       setErrorMessage(element[0], msg);
-      return;
+      return false;
   };
   hideErrorMessage(element[0]);
-  return;
+  return true;
 };
 
 // check if conditions are accepted
-function checkConditions(element, msg) {
+const checkConditions = (element, msg) => {
   if(!element.checked) {
       setErrorMessage(element, msg);
-      return;
+      return false;
   } 
   hideErrorMessage(element);
-  return;  
+  return true;  
 };
 
 const setErrorMessage = (element, msg) => {
@@ -131,6 +137,30 @@ const hideErrorMessage = element => {
   element.parentElement.removeAttribute('data-error-visible');
   element.parentElement.removeAttribute('data-error');
 };
+
+
+// submitting form
+form.addEventListener("submit", (event) => {
+    // On empêche le comportement par défaut
+    event.preventDefault();
+    const firstnameValid  = checkName(firstNameField, message.name);
+    const lastnameValid   = checkName(lastNameField, message.name);
+    const emailValid      = checkEmail(emailField, message.email);
+    const birthdateValid  = checkBirthdate(birthdateField, message.birthdate); 
+    const quantityValid   = checkQuantity(quantityField, message.quantity);
+    const cityValid       = checkCity(cityBtn, message.city);
+    const conditionsValid = checkConditions(conditionsBtn, message.conditions);
+    const formIsValid = firstnameValid && lastnameValid && emailValid 
+     && birthdateValid && quantityValid && cityValid & conditionsValid;
+     
+    if (formIsValid) {
+      // reset form, inform user, close form
+      alert("You are succesfully registered!");
+      form.reset();
+      modalbg.style.display = "none"
+    }
+
+  });
 
 
 
